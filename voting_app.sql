a35522c90948758523b68d79f2d1436a20cad273
mothishcodes <mothish302004@gmail.com>
2025-09-28 22:13:12 +0530

    voting_app.sql

--- voting_app.sql
+++ voting_app.sql
@@ -1,10 +1,28 @@
-CREATE DATABASE voting_app;
 
-USE voting_app;
+CREATE DATABASE online_voting;
+USE online_voting;
+
+
+
+CREATE TABLE voters (
+voter_id VARCHAR(64) PRIMARY KEY,
+name VARCHAR(255),
+has_voted TINYINT(1) DEFAULT 0,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 
 CREATE TABLE votes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    voter VARCHAR(100) UNIQUE NOT NULL,
-    candidate VARCHAR(100) NOT NULL,
-    vote_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+id INT AUTO_INCREMENT PRIMARY KEY,
+voter_id VARCHAR(64),
+candidate VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (voter_id) REFERENCES voters(voter_id)
 );
+
+
+INSERT INTO voters (voter_id, name) VALUES
+('VOTER001','Ram'),
+('VOTER002','Rahul'),
+('VOTER003','Mohit');
